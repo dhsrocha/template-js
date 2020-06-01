d382c349b8b3ad4445d75d0d962572f637013131
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-06-01 09:34:08 -0300

    Added Preact replacement for Next.js deployment.
    In order to lower the payload amount for a deployment.
    
    * Added Preact dependencies; and
    * Added Rpreact replacement configuration settings.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ module.exports = {
       ...config.resolve.alias,
       "@src": path.resolve(__dirname, "./src"),
       "@pages": path.resolve(__dirname, "./pages"),
+      // Replace React by Preact, suited to work only for web
+      // applications, in deployment. Since this repository is
+      // suposed to work under Next.js premises, this approach joins
+      // the framework and the library synergically.
+      react: "preact/compat",
+      "react-dom": "preact/compat",
+      "react-render-to-string": "preact-render-to-string",
+      "react-ssr-prepass": "preact-ssr-prepass",
     };
     return config;
   },
