6a6db47baa792f128399861dbe042a52c7265299
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-06-11 20:23:00 -0300

    Organized Input tests in subtopics.

--- src/components/Input/.test.tsx
+++ src/components/Input/.test.tsx
@@ -11,26 +11,32 @@ describe("Render tests.", () => {
     // Assert
     expect(subject.find("section")).toBeDefined();
   });
-  it("Should render one <text[type=text]/>.", async () => {
-    // Act
-    const subject = shallow(<Input />)
-      .find("input")
-      .find({ type: "text" });
-    // Assert
-    expect(subject).toHaveLength(1);
+  describe("Checkbox.", () => {
+    it("Should render one <text[type=checkbox]/>.", async () => {
+      // Act
+      const subject = shallow(<Input />)
+        .find("input")
+        .find({ type: "checkbox" });
+      // Assert
+      expect(subject).toHaveLength(1);
+    });
   });
-  it("Should render one <button>.", async () => {
-    // Act
-    const subject = shallow(<Input />).find("button");
-    // Assert
-    expect(subject).toHaveLength(1);
+  describe("Text.", () => {
+    it("Should render one <text[type=text]/>.", async () => {
+      // Act
+      const subject = shallow(<Input />)
+        .find("input")
+        .find({ type: "text" });
+      // Assert
+      expect(subject).toHaveLength(1);
+    });
   });
-  it("Should render one <text[type=checkbox]/>.", async () => {
-    // Act
-    const subject = shallow(<Input />)
-      .find("input")
-      .find({ type: "checkbox" });
-    // Assert
-    expect(subject).toHaveLength(1);
+  describe("Button.", () => {
+    it("Should render one <button>.", async () => {
+      // Act
+      const subject = shallow(<Input />).find("button");
+      // Assert
+      expect(subject).toHaveLength(1);
+    });
   });
 });
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from "react";
 export const Input = (): ReactElement => {
   return (
     <section>
+      <input type="checkbox" />
       <input type="text" />
       <button></button>
     </section>
