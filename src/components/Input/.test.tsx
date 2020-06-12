67471b0f283dac5bd46596bdb3d209d9dd280679
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-06-11 21:14:02 -0300

    Improved instrumentation usage in Todo component.

--- src/components/Input/.test.tsx
+++ src/components/Input/.test.tsx
@@ -5,19 +5,17 @@ import { shallow } from "enzyme";
  * @author Diego Rocha <dhsrocha.dev@gmail.com>
  */
 describe("Render tests.", () => {
+  // Arrange / Act
+  const wrap = shallow(<Input />);
   it("Should render <section>.", async () => {
-    // Act
-    const subject = shallow(<Input />);
     // Assert
-    expect(subject.find("section")).toBeDefined();
+    expect(wrap.find("section")).toBeDefined();
   });
   it("<section> should be the topmost component.", async () => {
     // Act
-    const subject = shallow(<Input />)
-      .find("section")
-      .props().children;
+    const child = wrap.find("section").props().children;
     // Assert
-    expect(subject).toEqual([
+    expect(child).toEqual([
       <input type="checkbox" />,
       <input type="text" />,
       <button />,
@@ -26,29 +24,25 @@ describe("Render tests.", () => {
   describe("Checkbox.", () => {
     it("Should render one <text[type=checkbox]/>.", async () => {
       // Act
-      const subject = shallow(<Input />)
-        .find("input")
-        .find({ type: "checkbox" });
+      const child = wrap.find("input").find({ type: "checkbox" });
       // Assert
-      expect(subject).toHaveLength(1);
+      expect(child).toHaveLength(1);
     });
   });
   describe("Text.", () => {
     it("Should render one <text[type=text]/>.", async () => {
       // Act
-      const subject = shallow(<Input />)
-        .find("input")
-        .find({ type: "text" });
+      const child = wrap.find("input").find({ type: "text" });
       // Assert
-      expect(subject).toHaveLength(1);
+      expect(child).toHaveLength(1);
     });
   });
   describe("Button.", () => {
     it("Should render one <button>.", async () => {
       // Act
-      const subject = shallow(<Input />).find("button");
+      const child = wrap.find("button");
       // Assert
-      expect(subject).toHaveLength(1);
+      expect(child).toHaveLength(1);
     });
   });
 });
