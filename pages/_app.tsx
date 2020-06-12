9a8d2f0e438f988083a6178b6b7fefff243d0309
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-06-11 23:38:55 -0300

    Added react-axe linting condiguration.
    @see https://github.com/dequelabs/react-axe/issues/125
    
    * Also added @types/react-axe as dependency.

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps as Props } from "next/app";
+import { ReactElement as RE } from "react";
+
+const isSSR = () => typeof window === "undefined";
+
+/**
+ * Accessibility tool: Outputs to devtools console on dev only and client-side only.
+ * @see https://github.com/dequelabs/react-axe/issues/125
+ */
+if (process.env.NODE_ENV !== "production" && !isSSR()) {
+  const React = require("react"); // eslint-disable-line
+  const DOM = require("react-dom"); // eslint-disable-line
+  const axe = require("react-axe"); // eslint-disable-line
+  axe(React, DOM, 1000);
+}
+
+export default function MyApp({ Component, pageProps }: Props): RE {
+  return <Component {...pageProps} />;
+}
