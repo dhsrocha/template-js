baa2368635580d26842baff0dbb40c20efa93a4a
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-06-24 10:59:18 -0300

    Added title to index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import { NextPage } from "next";
 import { Root } from "@src/Root";
+import { Head } from "next";
 
 /**
  * @author Diego Rocha <dhsrocha.dev@gmail.com>
  */
-const Index: NextPage = () => <Root />;
+const Index: NextPage = () => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+       <title>A robust template</title>
+    </Head>
+    <Root />
+  </>
+);
 
 export default Index;
